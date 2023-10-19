c5b11ab5eb2631fc12cf695170bd7ac0a45330dc
JAEJIN <wowls9416@naver.com>
2023-10-19 16:09:15 +0900

    검색 후에 팝업이 안되네,,

--- main.js
+++ main.js
@@ -9,10 +9,20 @@ const options = {
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
 .then(res => res.json())
 .then(res => {
+    // 영화 데이터
     const result = res.results;
-    const cardListWrap = document.getElementById('m_list_wrap');
     
     // 영화 데이터 불러와서 카드형태로 뿌리기
+    const cardListWrap = document.getElementById('m_list_wrap');
+    const cardLists = (post_img, title) => {
+        let temp_html = `
+        <div class="img_wrap">
+            <img src="https://image.tmdb.org/t/p/w500${post_img}" alt="post_img">
+        </div>
+        <p class="movie_title">${title}</p>
+        `;
+        return temp_html
+    }
     result.forEach(el => {
         // console.log(el);
         const post_image = el.poster_path;
@@ -22,25 +32,57 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
         const listItem = document.createElement('li'); // li 추가
         listItem.classList.add('m_list'); // li에 m_list 클래스 추가
         listItem.setAttribute('data-id', dataId);
-        let temp_html = `
-        <div class="img_wrap">
-            <img src="https://image.tmdb.org/t/p/w500${post_image}" alt="post_img">
-        </div>
-        <p class="movie_title">${title}</p>
-        `;
-        listItem.innerHTML = temp_html; // 추가한 li.m_list에 내용 넣기;
+        listItem.innerHTML = cardLists(post_image, title); // 추가한 li.m_list에 내용 넣기;
         
         cardListWrap.appendChild(listItem);
-
     });
-    
+
+
+    // 영화 타이틀로 검색
+    const searchInput = document.querySelector('#search');
+    const searchBtn = document.querySelector('#search_btn');
+    let searchList= [];
+    // input.value에 맞는 영화타이틀 filter
+    searchInput.addEventListener('input', () => {
+        const value = searchInput.value.toLowerCase();
+        searchList = result.filter(item=> {
+            const m_title = item.title.toLowerCase();
+            return m_title.includes(value) ? item : '';
+        });
+    });
+    // 검색버튼 클릭 시 filter list 노출
+    searchBtn.addEventListener('click', function() {
+        if(searchList.length === 0) {
+            alert('검색하신 영화가 없습니다, 다시 검색해주세요!');
+            searchInput.value= '';
+            searchInput.focus();
+            return;
+        }
+        cardListWrap.innerHTML = '';
+        searchList.forEach(item => {
+            const post_image = item.poster_path;
+            const title = item.title;
+            const dataId = item.id;
+            console.log(item);
+        
+            const listItem = document.createElement('li'); // li 추가
+            listItem.classList.add('m_list'); // li에 m_list 클래스 추가
+            listItem.setAttribute('data-id', dataId);
+            listItem.innerHTML = cardLists(post_image, title); // 추가한 li.m_list에 내용 넣기;
+            
+            cardListWrap.appendChild(listItem);
+        });
+    });
+
+
+
     // 해당 포스터 클릭 했을 때, 팝업창 데이터에 맞춰서 뜨기
     const cardList = document.querySelectorAll('.m_list'); // 카드리스트
     const popDimm = document.querySelector('.dimm'); // 팝업 배경
     const popup = document.querySelector('.popup_cont_wrap'); // 팝업 데이터 넣기
     const popOpen = document.querySelector('#popOpen'); // 팝업 열기
     const closeBtn = document.querySelector('.closeBtn'); // 팝업 close버튼
-    let popupOpen = (title, image, summaryCont, rating) => {
+    const popupOpen = (title, image, summaryCont, rating) => {
         const popup_html = `
             <div class="pop_cont">
                 <h2>${title}</h2>
@@ -68,7 +110,6 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
             popOpen.classList.add('_on');
         });
     });
-    
     // 팝업 닫기
     popDimm.addEventListener('click',()=>{
         popDimm.classList.remove('_on');
@@ -95,10 +136,11 @@ window.addEventListener("scroll", function() {
 
     if (scrollPosition >= 100) {
         // 현재 내 스크롤 위치가 100 초과 시
-        hederSearch.style.opacity = '0.9';
+        hederSearch.style.paddingTop = '20px';
     } else {
         // 현재 내 스크롤 위치가 100 미만 시
-        hederSearch.style.opacity = '0';
+        hederSearch.style.paddingTop = '0';
+        
     }
 });
 
